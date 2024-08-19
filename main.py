2f7a183626ba67012b94b29ada54c78eac92db4d
paelios <paelconstructs@gmail.com>
2024-08-19 15:42:19 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import pygame
+from asteroids.constants.py import *
+
+def main():
+    print("Starting asteroids!\n Screen width: {SCREEN_WIDTH}\n Screen height: 720")
+    pass
+    
+if __name__ == "__main__":
+    main()
